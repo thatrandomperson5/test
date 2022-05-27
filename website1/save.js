1a765b965572e18b6cc60379dc5aff85817f7691
thatrandomperson5 <94306853+thatrandomperson5@users.noreply.github.com>
2022-05-27 12:38:55 -0500

    Update save.js

--- website1/save.js
+++ website1/save.js
@@ -4,9 +4,11 @@
           const css = document.getElementById("css");
           console.log("html=" + html.value)
           document.getElementById("output").contentWindow.document.body.innerHTML = html.value
+          
+          output = document.getElementById("output").contentWindow.document.title
+          document.getElementById("minititle).textContent = output
+                                  
           document.cookie = "html=" + html.value;
           document.cookie = "javascript=" + js.value;
           document.cookie = "css=" + css.value;
-          output = document.getElementById("output").contentWindow.document.title
-          document.getElementById("minititle).textContent = output
         }
