dd8b01d80bf92aca09fd1cfc6a30dfd932afc041
thatrandomperson5 <94306853+thatrandomperson5@users.noreply.github.com>
2022-05-27 12:24:04 -0500

    Update save.js

--- website1/save.js
+++ website1/save.js
@@ -2,10 +2,9 @@
           const html = document.getElementById("html");
           const js = document.getElementById("javascript");
           const css = document.getElementById("css");
+          console.log("html=" + html.value)
+          document.getElementById("output").contentWindow.document.body.innerHTML = html
           document.cookie = "html=" + html.value;
           document.cookie = "javascript=" + js.value;
           document.cookie = "css=" + css.value;
-          console.log("html=" + html.value)
-          .contentWindow.document.body.innerHTML = html
-          document.getElementById("output").contentWindow.document.body.innerHTML = html
         }
